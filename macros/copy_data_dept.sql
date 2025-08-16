c5b9b66c7d83084264c45e59588aafb944ce3555
venkannagandu@gmail.com <venkannagandu@gmail.com>
2025-08-16 01:17:15 +0000

    Done

--- macros/copy_data_dept.sql
+++ macros/copy_data_dept.sql
@@ -1,49 +1,20 @@
 {% macro macros_copy_csv(table_nm) %} 
 
- 
+delete from {{ var('rawhist_db') }}.{{ var('wrk_schema') }}.{{ table_nm }};
 
-delete from {{var ('rawhist_db') }}.{{var ('wrk_schema')}}.{{ table_nm }};
-
- 
-
-COPY INTO {{var ('rawhist_db') }}.{{var ('wrk_schema')}}.{{ table_nm }} 
-
-FROM 
-
-(
-
-SELECT
-
-    $1 AS Store,
-
-    $2 AS Dept,
-
-    $3 AS Date1,
-
-    $4 AS Weekly_Sales,
-
-    $5 AS IsHoliday,
-    
-    CURRENT_TIMESTAMP() AS INSERT_DTS,
-
-    CURRENT_TIMESTAMP() AS UPDATE_DTS,
-
-    metadata$filename AS SOURCE_FILE_NAME,
-
-    metadata$file_row_number AS SOURCE_FILE_ROW_NUMBER
-
-FROM @{{ var('stage_name') }}
-
+COPY INTO {{ var('rawhist_db') }}.{{ var('wrk_schema') }}.{{ table_nm }} 
+FROM (
+    SELECT
+        $1::INT    AS STORE_ID,
+        $2::INT    AS DEPT_ID,
+        $3::DATE   AS SALES_DATE,
+        $4::NUMBER(10,2) AS WEEKLY_SALES,
+        $5::BOOLEAN AS IS_HOLIDAY
+    FROM @{{ var('stage_name') }}
 )
-
-FILE_FORMAT = {{var ('file_format_csv') }}
-
-PURGE={{ var('purge_status') }}
-
-FORCE = TRUE
-
-;
-
- 
+FILE_FORMAT = (FORMAT_NAME = {{ var('file_format_csv') }})
+PATTERN = 'department.csv' 
+PURGE = {{ var('purge_status') }}
+FORCE = TRUE;
 
 {% endmacro %}
